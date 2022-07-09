140f017fa9a24c903e688f2b3c9a438a038db785
Olive <olivelliott48@gmail.com>
2022-07-09 16:11:14 -0400

    Set up inquirer initial prompt, create functionality to view all departments, roles, and employees

--- server.js
+++ server.js
@@ -1,32 +1,112 @@
-const express = require("express");
+// const express = require("express");
 const nodemon = require("nodemon");
-const mysql = require("mysql2");
 const db = require("./db/connection");
-// const apiRoutes = require("./routes/apiRoutes");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// db.connect(err => {
+//     if (err) throw err;
+//     console.log('Connected here');
+//     startPrompt();
+// })
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// app.use("/api", apiRoutes);
-// * Testing database connection
-db.query(`SELECT * FROM departments`, (err, rows) => {
-    console.log(rows);
-})
-
-
-//*  Catch all route for error handling
-app.use((req, re) => {
-  res.status(404).end();
-});
-
-//* Start server after DB connection
 db.connect((err) => {
   if (err) throw err;
   console.log("Connected to the employee database ⭐");
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  startPrompt();
 });
+
+
+const promptArr = [
+    'View all departments',
+    'View all roles',
+    'View all employees',
+    'Add a department',
+    'Add a role',
+    'Add an employee',
+    'Update an employee role',
+    'Im done!'
+]
+
+startPrompt = function () {
+    inquirer.prompt({
+        type: 'list',
+        name: 'chosenAction',
+        message: 'What would you like to do?',
+        choices: promptArr
+    })
+    .then (({ chosenAction }) => {
+        switch (chosenAction) {
+            case 'View all departments':
+                showDepartments();
+                break;
+            
+            case 'View all roles':
+                viewRoles();
+                break;
+
+            case 'View all employees':
+                viewEmployees();
+                break;
+
+            case 'Add a department':
+                break;
+
+            case 'Add a role':
+                break;
+
+            case 'Add a employee':
+                break;
+
+            case 'Update an employee role':
+                break;
+
+            case 'Im done!':
+                console.log('Bye!');
+                process.exit();
+        }
+    });
+}
+
+// Show all departments
+const showDepartments = () => {
+    const sql = `SELECT * FROM departments`;
+    db.promise().query(sql)
+        .then(([rows, fields ]) => {
+            console.table(rows);
+            return startPrompt();
+        })
+        .catch(console.log);
+};
+
+// View all roles
+const viewRoles = () => {
+    const sql = `SELECT * FROM roles`;
+    db.promise().query(sql)
+      .then(([rows, fields]) => {
+        console.table(rows);
+        return startPrompt();
+      })
+      .catch(console.log);
+};
+
+// View all employees
+const viewEmployees = () => {
+    const sql = `SELECT * FROM employees`;
+    db.promise()
+      .query(sql)
+      .then(([rows, fields]) => {
+        console.table(rows);
+        return startPrompt();
+      })
+      .catch(console.log);
+};
+
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+
+                // db.promise().query(`INSERT INTO employees (name, department) VALUES (?, ?)`)
+
+
+
+
