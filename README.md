f95dfa9849cba146352f58091f73c372b6f55d47
Olive <olivelliott48@gmail.com>
2022-07-09 17:38:35 -0400

    Refactor code and create seperate js files for employee, role, and department functions

--- README.md
+++ README.md
@@ -11,8 +11,8 @@
 * THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 *WHEN I choose to add a department
 *THEN I am prompted to enter the name of the department and that department is added to the database
-WHEN I choose to add a role
-THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+*WHEN I choose to add a role
+*THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 WHEN I choose to add an employee
 THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 WHEN I choose to update an employee role
--- server.js
+++ server.js
@@ -4,17 +4,27 @@ const db = require("./db/connection");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
+const {
+    showDepartments,
+    addDepartment
+} = require('./lib/Departments');
+
+const {
+    viewRoles,
+    roleParams,
+} = require('./lib/Roles');
+
+
+const { viewEmployees } = require('./lib/Employee');
+
+
+
 
 db.connect((err) => {
   if (err) throw err;
   console.log("Connected to the employee database ⭐");
   startPrompt();
 });
-
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added 
-// to the database
-
 
 
 const promptArr = [
@@ -28,18 +38,6 @@ const promptArr = [
     'Im done!'
 ]
 
-const rolePromptArr = [
-    {
-        type: 'input',
-        name: 'roleName',
-        message: 'What is the name of the department you would like to add?'
-    },
-    {
-        type: 'input',
-        name: 'salary',
-        message: 'Please enter the salary of the role you are adding.'
-    },
-];
 
 startPrompt = function () {
     inquirer.prompt({
@@ -53,7 +51,7 @@ startPrompt = function () {
             case 'View all departments':
                 showDepartments();
                 break;
-            
+
             case 'View all roles':
                 viewRoles();
                 break;
@@ -81,117 +79,9 @@ startPrompt = function () {
                 process.exit();
         }
     });
-}
-
-// Show all departments
-const showDepartments = () => {
-    const sql = `SELECT * FROM departments`;
-    db.promise().query(sql)
-        .then(([rows, fields ]) => {
-            console.table(rows);
-            return startPrompt();
-        })
-        .catch(console.log);
-};
-
-// Add a new department
-const addDepartment = () => {
-  inquirer
-    .prompt({
-      type: "input",
-      name: "deptName",
-      message: "Enter the name of the department you would like to add",
-    })
-    .then((answer) => {
-      const sql = `INSERT INTO departments (dept_name) VALUES (?)`;
-      // const params = deptName.trim();
-      db.query(sql, answer.deptName, (err, result) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(`Added ${answer.deptName} to the database!`);
-          showDepartments();
-        }
-      });
-    });
-};
-
-// View all roles
-const viewRoles = () => {
-    const sql = `SELECT * FROM roles`;
-    db.promise().query(sql)
-      .then(([rows, fields]) => {
-        console.table(rows);
-        return startPrompt();
-      })
-      .catch(console.log);
 };
 
 
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added 
-// to the database
-
-// Add new role
-const roleParams = () => {
-    inquirer.prompt(rolePromptArr)
-    .then((answers) => {
-        const params = [answers.roleName, answers.salary];
-
-        //* get the department id from the department table
-        const deptSql = `SELECT id, dept_name FROM departments`;
-
-        db.query(deptSql, (err, deptData) => {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                const deptArr = deptData.map(({ id, dept_name }) => ({ name: dept_name, value: id }));
-                inquirer.prompt([
-                    {
-                        type: 'list',
-                        name: 'department',
-                        message: 'What department is this role in?',
-                        choices: deptArr
-                    }
-                ])
-                .then(deptChoice => {
-                    const newRoleDept = deptChoice.department;
-                    params.push(newRoleDept);
-
-                    addNewRole(params);
-                })
-            };
-        });
-    });
-};
-
-const addNewRole = params => {
-    const sql = `INSERT INTO roles (job_title, salary, department_id)
-                VALUES (?, ?, ?)`;
-
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(`Added to roles`);
-            viewRoles();
-        }
-    });
-
-}
-
-// View all employees
-const viewEmployees = () => {
-    const sql = `SELECT * FROM employees`;
-    db.promise()
-      .query(sql)
-      .then(([rows, fields]) => {
-        console.table(rows);
-        return startPrompt();
-      })
-      .catch(console.log);
-};
 
 
 
