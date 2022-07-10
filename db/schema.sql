f2e0eb636f4b7ab18bc63c690bd309c1f9f8d491
Olive <olivelliott48@gmail.com>
2022-07-10 17:25:07 -0400

    Update sql query to view all employees

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE roles (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     job_title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    department_id INTEGER NOT NULL
+    department_id INTEGER NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employees (
@@ -20,5 +21,9 @@ CREATE TABLE employees (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
-    manager_id INTEGER
+    manager_id INTEGER,
+    INDEX manager_ind (manager_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
+
 );
